27a613c489a5bb6aa75df6980750e1aae660054c
mcfangpy <mcfangpy@gmail.com>
2023-10-27 18:24:39 +0800

    init party code

--- main.go
+++ main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"fmt"
 
+	"github.com/bnb-chain/tss-lib/ecdsa/keygen"
 	"github.com/bnb-chain/tss-lib/tss"
 )
 
 func main() {
 	fmt.Println("Hello, Go!")
 
-	parties := make([]*tss.Party, 3)
-
-	for i := range parties {
-		fmt.Println("party", i)
+	qty := 3
+	keys := make([]keygen.LocalPartySaveData, 0, qty)
+	start := 0
+	for i := start; i < qty; i++ {
+		var key keygen.LocalPartySaveData
+		for _, kbxj := range key.BigXj {
+			kbxj.SetCurve(tss.S256())
+		}
+		key.ECDSAPub.SetCurve(tss.S256())
+		keys = append(keys, key)
 	}
+	partyIDs := make(tss.UnSortedPartyIDs, len(keys))
+	for i, key := range keys {
+		pMoniker := fmt.Sprintf("%d", i+start+1)
+		partyIDs[i] = tss.NewPartyID(pMoniker, pMoniker, key.ShareID)
+	}
+	sortedPIDs := tss.SortPartyIDs(partyIDs)
 }
