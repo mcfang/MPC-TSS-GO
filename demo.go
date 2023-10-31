bd532dd4cb46cc48b179bec7ccbba0fc211b530b
mcfangpy <mcfangpy@gmail.com>
2023-10-31 18:12:03 +0800

    use different channels

--- demo.go
+++ demo.go
@@ -1,86 +1,79 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-	"sync/atomic"
-
-	"github.com/bnb-chain/tss-lib/common"
-	"github.com/bnb-chain/tss-lib/ecdsa/keygen"
-	"github.com/bnb-chain/tss-lib/test"
-	"github.com/bnb-chain/tss-lib/tss"
-)
-
 func demo() {
 	clientPartyIDs, clientKeys := clientInit()
 	serverPartyIDs, serverKeys := serverInit()
 	partyIDs := append(clientPartyIDs, serverPartyIDs...)
 	keys := append(clientKeys, serverKeys...)
-	pIDs := tss.SortPartyIDs(partyIDs)
-	p2pCtx := tss.NewPeerContext(pIDs)
-	parties := make([]*keygen.LocalParty, 0, len(pIDs))
 
-	errCh := make(chan *tss.Error, len(pIDs))
-	outCh := make(chan tss.Message, len(pIDs))
-	endCh := make(chan keygen.LocalPartySaveData, len(pIDs))
+	clientRun(partyIDs, keys)
+	serverRun(partyIDs, keys)
 
-	updater := test.SharedPartyUpdater
+	// 	pIDs := tss.SortPartyIDs(partyIDs)
+	// 	p2pCtx := tss.NewPeerContext(pIDs)
+	// 	parties := make([]*keygen.LocalParty, 0, len(pIDs))
 
-	startGR := runtime.NumGoroutine()
+	// 	errCh := make(chan *tss.Error, len(pIDs))
+	// 	outCh := make(chan tss.Message, len(pIDs))
+	// 	endCh := make(chan keygen.LocalPartySaveData, len(pIDs))
 
-	// init the parties
-	for i := 0; i < len(pIDs); i++ {
-		var P *keygen.LocalParty
-		params := tss.NewParameters(tss.S256(), p2pCtx, pIDs[i], len(pIDs), testThreshold)
-		P = keygen.NewLocalParty(params, outCh, endCh, keys[i].LocalPreParams).(*keygen.LocalParty)
-		parties = append(parties, P)
-		go func(P *keygen.LocalParty) {
-			if err := P.Start(); err != nil {
-				errCh <- err
-			}
-		}(P)
-	}
+	// 	updater := test.SharedPartyUpdater
 
-	// PHASE: keygen
-	var ended int32
-keygen:
-	for {
-		fmt.Printf("ACTIVE GOROUTINES: %d\n", runtime.NumGoroutine())
-		select {
-		case err := <-errCh:
-			common.Logger.Errorf("Error: %s", err)
-			break keygen
+	// 	startGR := runtime.NumGoroutine()
 
-		case msg := <-outCh:
-			dest := msg.GetTo()
-			if dest == nil { // broadcast!
-				for _, P := range parties {
-					if P.PartyID().Index == msg.GetFrom().Index {
-						continue
-					}
-					go updater(P, msg, errCh)
-				}
-			} else { // point-to-point!
-				if dest[0].Index == msg.GetFrom().Index {
-					fmt.Errorf("party %d tried to send a message to itself (%d)", dest[0].Index, msg.GetFrom().Index)
-					return
-				}
-				go updater(parties[dest[0].Index], msg, errCh)
-			}
+	// 	// init the parties
+	// 	for i := 0; i < len(pIDs); i++ {
+	// 		var P *keygen.LocalParty
+	// 		params := tss.NewParameters(tss.S256(), p2pCtx, pIDs[i], len(pIDs), testThreshold)
+	// 		P = keygen.NewLocalParty(params, outCh, endCh, keys[i].LocalPreParams).(*keygen.LocalParty)
+	// 		parties = append(parties, P)
+	// 		go func(P *keygen.LocalParty) {
+	// 			if err := P.Start(); err != nil {
+	// 				errCh <- err
+	// 			}
+	// 		}(P)
+	// 	}
 
-		case save := <-endCh:
-			// SAVE a test fixture file for this P (if it doesn't already exist)
-			// .. here comes a workaround to recover this party's index (it was removed from save data)
-			index, _ := save.OriginalIndex()
-			fmt.Printf("Party %d private key share (Xi): %s\n", index, save.LocalSecrets.Xi.String())
-			atomic.AddInt32(&ended, 1)
-			if atomic.LoadInt32(&ended) == int32(len(pIDs)) {
-				fmt.Printf("Done. Received save data from %d participants", ended)
-				fmt.Printf("Start goroutines: %d, End goroutines: %d", startGR, runtime.NumGoroutine())
-				break keygen
-			}
-		}
-	}
+	// 	// PHASE: keygen
+	// 	var ended int32
+	// keygen:
+	// 	for {
+	// 		fmt.Printf("ACTIVE GOROUTINES: %d\n", runtime.NumGoroutine())
+	// 		select {
+	// 		case err := <-errCh:
+	// 			common.Logger.Errorf("Error: %s", err)
+	// 			break keygen
+
+	// 		case msg := <-outCh:
+	// 			dest := msg.GetTo()
+	// 			if dest == nil { // broadcast!
+	// 				for _, P := range parties {
+	// 					if P.PartyID().Index == msg.GetFrom().Index {
+	// 						continue
+	// 					}
+	// 					go updater(P, msg, errCh)
+	// 				}
+	// 			} else { // point-to-point!
+	// 				if dest[0].Index == msg.GetFrom().Index {
+	// 					fmt.Errorf("party %d tried to send a message to itself (%d)", dest[0].Index, msg.GetFrom().Index)
+	// 					return
+	// 				}
+	// 				go updater(parties[dest[0].Index], msg, errCh)
+	// 			}
+
+	// 		case save := <-endCh:
+	// 			// SAVE a test fixture file for this P (if it doesn't already exist)
+	// 			// .. here comes a workaround to recover this party's index (it was removed from save data)
+	// 			index, _ := save.OriginalIndex()
+	// 			fmt.Printf("Party %d private key share (Xi): %s\n", index, save.LocalSecrets.Xi.String())
+	// 			atomic.AddInt32(&ended, 1)
+	// 			if atomic.LoadInt32(&ended) == int32(len(pIDs)) {
+	// 				fmt.Printf("Done. Received save data from %d participants", ended)
+	// 				fmt.Printf("Start goroutines: %d, End goroutines: %d", startGR, runtime.NumGoroutine())
+	// 				break keygen
+	// 			}
+	// 		}
+	// 	}
 }
 
 func main() {
